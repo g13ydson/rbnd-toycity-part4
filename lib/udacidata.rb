cbc6acd5c1c4a29672709536c37a7d530168803a
Gleydson Tavares <tavares.gleydson@gmail.com>
2016-06-29 19:36:55 -0300

    created all method

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -3,5 +3,33 @@ require_relative 'errors'
 require 'csv'
 
 class Udacidata
-  # Your code goes here!
+  @@data_path = File.dirname(__FILE__) + "/../data/data.csv"
+
+  def self.create(options = {})
+    db = CSV.read(@@data_path)
+
+    is_product_update = options[:id] ? true : false
+
+    prod = Product.new(options)
+    CSV.open(@@data_path, 'wb') do |csv|
+      db.each do |data|
+      	csv = is_product_update && data[0].to_i == prod.id ? csv << [prod.id, prod.brand, prod.name, prod.price] :  csv << data
+      end
+      unless is_product_update
+        csv << [prod.id, prod.brand, prod.name, prod.price]
+      end
+    end
+
+    return prod
+  end
+
+  def self.all
+    all_products = []
+    CSV.foreach( @@data_path, headers: true ) do |prod|
+      all_products << Product.new( id: prod["id"], brand: prod["brand"], name: prod["product"], price: prod["price"] )
+    end
+    all_products
+  end
+
+
 end
